9fc8287a4218b53d42b6e7b5c8ba03953d2b7636
SergejBros <sergej.bros@gmail.com>
2018-12-16 12:35:14 +0200

    файлы урока 7, создали форму регистрации, функции и сохраняем данные пользователей в файл

--- form.html.php
+++ form.html.php
@@ -5,12 +5,24 @@
     <title>Регистрация</title>
 </head>
 <body>
+    <?php if($userAdded): ?>
+    <p> Спасибо за регистрацию </p>
+    <?php else: ?>
     <form action="index.php" method="POST">
-        <label>Имя <input type="text" name="name"></label><?= $errors['name'] ?: '' ?><br>
-        <label>Email <input type="email" name="email"></label><?= $errors['email'] ?: '' ?><br>
-        <label>Пароль <input type="password" name="password"></label><?= $errors['password'] ?: '' ?><br>
-        <label><input type="checkbox" name="subscribe" value="1"> Подписаться на рассылку</label><br>
+        <label>Имя <input type="text" name="name" value="<?=$values['name']?>"></label>
+        <?= isset($errors['name']) ? $errors['name'] : '' ?><br>
+
+        <label>Email <input type="email" name="email" value="<?=$values['email']?>"></label>
+        <?= isset($errors['email']) ? $errors['email'] : '' ?><br>
+
+        <label>Пароль <input type="password" name="password" value="<?=$values['password']?>"></label>
+        <?= isset($errors['password']) ? $errors['password'] : '' ?><br>
+
+        <label><input type="checkbox" name="subscribe" value="1" <?=$values['subscribe'] ? 'checked' : ''?>>
+            Подписаться на рассылку
+        </label>    <br>
         <button type="submit">Зарегистрироваться</button>
     </form>
+    <?php endif ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,19 +1,37 @@
 <?php
 
-var_dump($_POST);
+require 'function.php';
+
 $errors = [];
+
+$values = [
+    'name' => '',
+    'email' => '',
+    'password' => '',
+    'subscribe' => 0
+];
+$userAdded = false;
+
 if(isset($_POST)){
 
-    if($_POST['name']){
-        $errors['name'] = 'Введите имя';
-    }
-    if($_POST['email']){
-        $errors['email'] = 'Введите емейл';
-    }
-    if($_POST['password']){
-        $errors['password'] = 'Введите пароль';
-    }
-}
+    checkEmpty('name', 'Введите имя');
+    checkEmpty('email', 'Введите емейл');
+    checkEmpty('password', 'Введите пароль');
 
+    if(empty($errors['email']) && strpos($_POST['email'], '@') === false){
+            $errors['email'] = 'Email должен содержать @';
+    }
+
+    if(!empty($_POST['subscribe'])){
+        $values['subscribe'] = 1;
+
+    }
+
+    if(!$errors){
+        saveUser($values);
+        $userAdded = true;
+    }
+
+}
 
 include 'form.html.php';
